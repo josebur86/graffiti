b412f5f5702da56f3aa2a20f36b96620efea6cea
Drew Ryder <drewjryder@gmail.com>
2015-05-26 22:04:15 -0500

    Make it easier to debug location probabilities on the go

--- app/src/main/java/com/tinydino/graffiti/MapActivity.java
+++ app/src/main/java/com/tinydino/graffiti/MapActivity.java
@@ -3,6 +3,7 @@ package com.tinydino.graffiti;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -17,15 +18,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient mGoogleApiClient;
     private String DEBUG_TAG = "DEBUGTAG";
+    private Toast _toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        _toast = Toast.makeText(this, "", Toast.LENGTH_LONG);
 
         MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -44,11 +49,21 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
             @Override
             public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                Log.d(DEBUG_TAG, "Entering result callback" + likelyPlaces.getCount());
+                //_toast.setText(String.format("Number of likely places: %s", likelyPlaces.getCount()));
+                //_toast.show();
+
+                ArrayList<PlaceLikelihood> likelyPlacesList = new ArrayList<PlaceLikelihood>();
                 for (PlaceLikelihood placeLikelihood : likelyPlaces) {
                     Log.d(DEBUG_TAG, String.format("Place '%s' has likelihood: %g",
                             placeLikelihood.getPlace().getName(),
                             placeLikelihood.getLikelihood()));
+
+                    likelyPlacesList.add(placeLikelihood);
                 }
+
+                _toast.setText(String.format("%s", likelyPlacesList.get(0).getPlace().getName() + "..." + likelyPlacesList.get(0).getLikelihood()));
+                _toast.show();
                 likelyPlaces.release();
             }
         });
