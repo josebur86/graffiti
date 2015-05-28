dc294991ceeba0a5e87a20429cca344590744090
Joe Burns <josebur86@gmail.com>
2015-05-27 20:41:45 -0500

    Project file change to set up unit testing.

---
+++ app/src/test/java/presenter/MessageBoardPresenterTest.java
@@ -0,0 +1,12 @@
+package com.tinydino.graffiti.test.presenter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MessageBoardPresenterTest {
+    @Test
+    public void foo() {
+        assertTrue(true);
+    }
+}
