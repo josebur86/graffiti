e57522e0762bce2baad01a2133961f3542f50a52
Jonathan Etters <ettersj@gmail.com>
2015-06-14 22:12:33 -0500

    Create identity client and start sign in, connection to rest of grafitti broken for now

--- app/src/main/java/com/tinydino/graffiti/ui/activity/UserLoginActivity.java
+++ app/src/main/java/com/tinydino/graffiti/ui/activity/UserLoginActivity.java
@@ -9,11 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.identitytoolkit.GitkitClient;
+import com.google.identitytoolkit.GitkitUser;
+import com.google.identitytoolkit.IdToken;
 import com.tinydino.graffiti.R;
 
 
 public class UserLoginActivity extends ActionBarActivity {
 
+    private GitkitClient client;
     private TextView userNameEditText;
     private Button loginButton;
     private final static String _defaultServerUri = "https://thawing-island-7364.herokuapp.com/";
@@ -21,15 +25,37 @@ public class UserLoginActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        GitkitClient.SignInCallbacks callbacks = new GitkitClient.SignInCallbacks() {
+            // Implement the onSignIn method of GitkitClient.SignInCallbacks interface.
+            @Override
+            public void onSignIn(IdToken idToken, GitkitUser gitkitUser) {
+                // Send the idToken to the server. The server should issue a session cookie/token
+                // for the app if the idToken is verified.
+                // authenticate(idToken.getTokenString());
+            }
+
+            // Implement the onSignInFailed method of GitkitClient.SignInCallbacks interface.
+            @Override
+            public void onSignInFailed() {
+                // Handle the sign in failure.
+            }
+        };
+
         setContentView(R.layout.activity_user_login);
         userNameEditText = (TextView)findViewById(R.id.userNameTextEdit);
         loginButton = (Button)findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+        client = GitkitClient.newBuilder(this, callbacks).build();
+        client.startSignIn();
+
+        /*loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doLogin(v);
             }
-        });
+        }); */
+
     }
 
     public void doLogin(View view) {
@@ -44,6 +70,22 @@ public class UserLoginActivity extends ActionBarActivity {
         messageBoardIntent.setClass(this, MessageBoardActivity.class);
 
         startActivity(messageBoardIntent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (client.handleIntent(intent)) {
+            // intent is handled by the GitkitClient.
+            return;
+        }
+    }
+
+    @Override
+     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (client.handleActivityResult(requestCode, resultCode, data)) {
+            // result is handled by GitkitClient.
+            return;
+        }
     }
 
     @Override
